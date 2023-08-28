8a3e8cf7d973965e5df1b7f7313f14add85323ab
dorian-adams <Dorian.Adams@dacoded.com>
2023-08-28 16:32:50 -0400

    Add documentation

--- apps/blog/validators.py
+++ apps/blog/validators.py
@@ -4,11 +4,13 @@ from apps.base.reddit_api import subreddit_found
 
 
 def validate_subreddit_format(subreddit):
+    # Must be alphanumeric
     if not subreddit.isalnum():
         raise ValidationError("Subreddit name cannnot contain any special characters.")
 
 
 def validate_subreddit_exists(subreddit):
+    # Subreddit must already exist
     if not subreddit_found(subreddit):
         raise ValidationError(
             "Subreddit was not found, please check the name or try again later."
